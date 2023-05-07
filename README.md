aee3e29c80f8735dc420a8921dfa20e644e73e11
Devin <guozwhero@126.com>
2023-05-07 11:15:47 +0800

    666

---
+++ README.md
@@ -0,0 +1,40 @@
+# TheGraph Subgraph
+
+## 合约
+
+```html
+todo ‼️ 因为 thegraph 现在还不支持 sepolia测试网络，所以改用goerli测试网络，但是goerli gas超高且测试币非常难领，
+所以作业实现方式：（1）把自己的Crowdfunding合约部署到sepolia，并开源合约源码，（2）自己写得thegraph调用其他同学已经成功部署到goerli测试网的合约，监听bid事件。
+
+- 我写的合约：
+0x62ABd112bE92cb29c46aE1e895817Fe813FC4BC8
+https://sepolia.etherscan.io/address/0x62ABd112bE92cb29c46aE1e895817Fe813FC4BC8#writeContract
+
+- 调用别人的合约位置
+0xE6fa0fd74ACB333E9714B245E5149b4DA2bbba11
+https://goerli.etherscan.io/address/0xE6fa0fd74ACB333E9714B245E5149b4DA2bbba11#code
+
+```
+
+## 演示
+
+### >>> step 1 页面展示
+
+![](https://github.com/guozhouwei/tmp/blob/main/images/001.png)
+
+### >>> step 2 代币初始值
+先部署代币合约到sepolia测试网络，获取合约地址，导入小狐狸钱包。
+![](https://github.com/guozhouwei/tmp/blob/main/images/01.png)
+如上图：代币有0.09999ADT
+### >>> step 3 mint
+铸造 3*1000000000000000000个代币，如下图：
+![](https://github.com/guozhouwei/tmp/blob/main/images/002.png)
+如上图：代币变为3.09999ADT
+
+### >>> step 4 burn
+燃烧 2*1000000000000000000个代币，如下图：
+![](https://github.com/guozhouwei/tmp/blob/main/images/003.png)
+如上图：代币变为1.09999ADT
+
+### >>> step 5 totalSupply
+![](https://github.com/guozhouwei/tmp/blob/main/images/004.png)
--- src/crowd-funding.ts
+++ src/crowd-funding.ts
@@ -1,35 +1,51 @@
-import {
-  CampaignBidLog as CampaignBidLogEvent,
-  CampaignLog as CampaignLogEvent
-} from "../generated/CrowdFunding/CrowdFunding"
-import { CampaignBidLog, CampaignLog } from "../generated/schema"
+// import { CampaignBidLog } from '../generated/CrowdFunding/CrowdFunding'
+import { BidLog } from '../generated/CrowdFunding/CrowdFunding'
+import { Funder } from '../generated/schema'
+import {Address, BigInt} from "@graphprotocol/graph-ts";
 
-export function handleCampaignBidLog(event: CampaignBidLogEvent): void {
-  let entity = new CampaignBidLog(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.campaignID = event.params.campaignID
-  entity.addr = event.params.addr
-  entity.amount = event.params.amount
+export function handleCampaignBidLog(event: BidLog): void {
+  let funder = Funder.load(event.params.campaignID.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  if (funder == null) {
+    funder = new Funder(event.params.campaignID.toString());
+    funder.bidder = event.params.bidder as Address;
+    funder.bidAmount = event.params.bidAmount as BigInt;
+    funder.save();
+  }
 }
 
-export function handleCampaignLog(event: CampaignLogEvent): void {
-  let entity = new CampaignLog(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.campaignID = event.params.campaignID
-  entity.receiver = event.params.receiver
-  entity.goal = event.params.goal
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+// import {
+//   CampaignBidLog as CampaignBidLogEvent,
+//   CampaignLog as CampaignLogEvent
+// } from "../generated/CrowdFunding/CrowdFunding"
+// import { CampaignBidLog, CampaignLog } from "../generated/schema"
+//
+// export function handleCampaignBidLog(event: CampaignBidLogEvent): void {
+//   let entity = new CampaignBidLog(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.campaignID = event.params.campaignID
+//   entity.addr = event.params.addr
+//   entity.amount = event.params.amount
+//
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
+//
+//   entity.save()
+// }
+//
+// export function handleCampaignLog(event: CampaignLogEvent): void {
+//   let entity = new CampaignLog(
+//     event.transaction.hash.concatI32(event.logIndex.toI32())
+//   )
+//   entity.campaignID = event.params.campaignID
+//   entity.receiver = event.params.receiver
+//   entity.goal = event.params.goal
+//
+//   entity.blockNumber = event.block.number
+//   entity.blockTimestamp = event.block.timestamp
+//   entity.transactionHash = event.transaction.hash
+//
+//   entity.save()
+// }
